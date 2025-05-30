5019b678ded94a8a98e2346ab9e55d4d6c958ef8
Jofresh77 <56953251+Jofresh77@users.noreply.github.com>
2025-05-30 16:51:44 +0200

    Corrupted project backup
     - add: custom state tree + state tree schema (corrupted)
     - add: system prompt data asset w/ multiline
     - corrupted: MVVM + HUD + TurnEndBtn + GM

---
+++ Source/MasterArbeit/AI/STTasks/STTConstructCharacterPrompt.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "STTConstructCharacterPrompt.h"
+
+EStateTreeRunStatus USTTConstructCharacterPrompt::EnterState(FStateTreeExecutionContext& Context,
+	const FStateTreeTransitionResult& Transition)
+{
+	//Super::EnterState(Context, Transition);
+	UE_LOG(LogTemp, Error, TEXT("%s"), *SystemPromptDataAsset->SystemPrompt.ToString());
+	return EStateTreeRunStatus::Succeeded;
+}
---
+++ Source/MasterArbeit/AI/StateTreeLlmAgentComponentSchema.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StateTreeComponentSchema.h"
+#include "Memory/AgentMemoryDataAssets.h"
+
+#include "StateTreeLlmAgentComponentSchema.generated.h"
+
+class AAIController;
+
+/**
+ * 
+ */
+UCLASS(BlueprintType, EditInlineNew, CollapseCategories,
+	meta = (DisplayName = "StateTree Llm Agent Component", CommonSchema))
+class MASTERARBEIT_API UStateTreeLlmAgentComponentSchema : public UStateTreeComponentSchema
+{
+	GENERATED_BODY()
+
+public:
+	UStateTreeLlmAgentComponentSchema(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	virtual void PostLoad() override;
+
+	virtual bool IsStructAllowed(const UScriptStruct* InScriptStruct) const override;
+
+	static bool SetContextRequirements(UBrainComponent& BrainComponent, FStateTreeExecutionContext& Context,
+	                                   bool bLogErrors = false);
+
+#if WITH_EDITOR
+	virtual void PostEditChangeChainProperty(struct FPropertyChangedChainEvent& PropertyChangedEvent) override;
+#endif
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Defaults", NoClear)
+	TSubclassOf<AAIController> AIControllerClass = nullptr;
+
+	UPROPERTY(EditAnywhere, Category = "Defaults", NoClear)
+	USystemPromptDataAsset* SystemPromptDataAsset = nullptr;
+};
