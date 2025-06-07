3afff3b15e8069b10f5b7f16c9ce043e4102a123
Jofresh77 <56953251+Jofresh77@users.noreply.github.com>
2025-06-08 01:03:52 +0200

    WIP: Chat system (player-side)
     - add: custom turn cost gameplay ability class (contains PA cost)
     - add: speech ability (add to textcom sub) has cost checks and works with event (WIP)
     - add: actions and movement actions costs GE (used in speech ability)
     - add: text communication subsystem
     - change: ChatPannel to include a multi-line text block input (broadcast on enter pressed)
     - change: HUD to listen to enter pressed (-> interact with textcom sub)
     - change: HUD to listen to new message in textcom sub (-> call create chat message BPW in ChatPanel)

---
+++ Source/MasterArbeit/TextCommunication/TextCommunicationSubsystem.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TextCommunicationSubsystem.h"
+
+UCharacterChatMessageWrapper::UCharacterChatMessageWrapper()
+{
+	
+}
+
+void UTextCommunicationSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+	CharacterChatMessages = NewObject<UDataTable>(this);
+	CharacterChatMessages->RowStruct = FCharacterChatMessage::StaticStruct();
+
+	
+}
+
+void UTextCommunicationSubsystem::AddCharacterChatMessage(FCharacterChatMessage CharacterChatMessage)
+{
+	if (!IsValid(CharacterChatMessages))
+	{
+		UE_LOG(LogTemp, Error, TEXT("CharacterChatMessages DataTable is invalid"));
+		return;
+	}
+
+	if (CharacterChatMessage.Message.IsEmpty())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Cannot add empty message"));
+		return;
+	}
+
+	FString RowName = FString::Printf(TEXT("%s_%lld"), 
+		*UEnum::GetValueAsString(CharacterChatMessage.CharacterName.GetValue()),
+		FDateTime::Now().GetTicks());
+
+	CharacterChatMessages->AddRow(FName(*RowName), CharacterChatMessage);
+
+	OnNewCharacterChatMessage.Broadcast(CharacterChatMessage);
+}
---
+++ Source/TurnBasedCombatSystem/Private/GMMCTurnPointsCost.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GMMCTurnPointsCost.h"
+
+#include "TurnCostGameplayAbility.h"
+
+float UGMMCTurnPointsCost::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	const UTurnCostGameplayAbility* Ability = Cast<UTurnCostGameplayAbility>(Spec.GetContext().GetAbility());
+
+	if (Ability == NULL)
+	{
+		return 0.f;
+	}
+
+	return Ability->Cost;
+}
