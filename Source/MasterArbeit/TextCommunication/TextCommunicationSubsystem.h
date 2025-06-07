79798c225d0d7fcf717d0cfd97e90b3fc01b8df8
Jofresh77 <56953251+Jofresh77@users.noreply.github.com>
2025-06-08 01:03:52 +0200

    WIP: Chat system (player-side)
     - add: custom turn cost gameplay ability class (contains PA cost)
     - add: speech ability (add to textcom sub) has cost checks and works with event (WIP)
     - add: actions and movement actions costs GE (used in speech ability)
     - add: text communication subsystem
     - change: ChatPannel to include a multi-line text block input (broadcast on enter pressed)
     - change: HUD to listen to enter pressed (-> interact with textcom sub)
     - change: HUD to listen to new message in textcom sub (-> call create chat message BPW in ChatPanel)

---
+++ Source/MasterArbeit/TextCommunication/TextCommunicationSubsystem.h
@@ -0,0 +1,61 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MasterArbeit/MasterArbeitCharacter.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "TextCommunicationSubsystem.generated.h"
+
+USTRUCT(BlueprintType, Category="TextCommunication | UIChat")
+struct FCharacterChatMessage : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="TextCommunication | UIChat")
+	TEnumAsByte<ECharacterGroupName> CharacterName = Default; //TODO: should be changed to concrete names
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="TextCommunication | UIChat", meta=(MultiLine))
+	FText Message;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="TextCommunication | UIChat")
+	FLinearColor ThemeColor = FLinearColor::White;
+};
+
+UCLASS(Blueprintable, Category="TextCommunication | AbilityPayload")
+class UCharacterChatMessageWrapper : public UObject
+{
+	GENERATED_BODY()
+
+	UCharacterChatMessageWrapper();
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="TextCommunication | AbilityPayload")
+	FCharacterChatMessage CharacterChatMessage;
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnNewCharacterChatMessage, const FCharacterChatMessage&,
+                                            CharacterChatMessage);
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, Category="TextCommunication")
+class MASTERARBEIT_API UTextCommunicationSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable, Category="TextCommunication")
+	FOnNewCharacterChatMessage OnNewCharacterChatMessage;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="TextCommunication",
+		meta=(RequiredAssetDataTags="RowStructure=/Script/MasterArbeit.FCharacterChatMessage"))
+	UDataTable* CharacterChatMessages;
+
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+	UFUNCTION(BlueprintCallable, Category="TextCommunication")
+	void AddCharacterChatMessage(FCharacterChatMessage CharacterChatMessage);
+};
---
+++ Source/TurnBasedCombatSystem/Public/TurnCostGameplayAbility.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "TurnCostGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TURNBASEDCOMBATSYSTEM_API UTurnCostGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float Cost;
+};
