81da23d9da85025d76fdd2529f4d35ff52e834d4
Jofresh77 <56953251+Jofresh77@users.noreply.github.com>
2025-07-16 09:16:58 +0200

    Fix: circular GM dependencY (made a copy BP because nothing else was working)

---
+++ Source/MasterArbeit/GMInGame.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GMInGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MASTERARBEIT_API AGMInGame : public AGameModeBase
+{
+	GENERATED_BODY()
+
+	AGMInGame(const FObjectInitializer& ObjectInitializer);
+};
