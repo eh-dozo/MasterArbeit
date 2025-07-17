05e6df3ab1fdbd1c64ca4dd29df95b03a298c040
Jofresh77 <56953251+Jofresh77@users.noreply.github.com>
2025-07-17 20:50:54 +0200

    Landscape + fixes
    - add many libs from fab (mainly materials) [GITIGNORED]
    - add cloud shadows on terrain [GITIGNORED]
    - change: landscape material with auto generated (custom modified with road + grid)
    - attempt to fix circular dependencies (WIP)

---
+++ Source/MasterArbeit/CharacterTurnStart.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MasterArbeitCharacter.h"
+#include "Engine/NavigationObjectBase.h"
+#include "CharacterTurnStart.generated.h"
+
+UCLASS()
+class MASTERARBEIT_API ACharacterTurnStart : public ANavigationObjectBase
+{
+	GENERATED_BODY()
+
+public:
+	ACharacterTurnStart(const FObjectInitializer& ObjectInitializer);
+
+	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = CharacterStart)
+	TEnumAsByte<ECharacterGroupName> CharacterGroupName = Default;
+
+	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = CharacterStart)
+	int32 StartTurnIndex = 0;
+
+#if WITH_EDITORONLY_DATA
+private:
+	UPROPERTY()
+	TObjectPtr<class UArrowComponent> ArrowComponent;
+#endif
+};
