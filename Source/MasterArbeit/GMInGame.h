47a05b964cfa0dfdd050fc84944bb6678f92f8d2
Jofresh77 <56953251+Jofresh77@users.noreply.github.com>
2025-07-17 20:50:54 +0200

    Landscape + fixes
    - add many libs from fab (mainly materials) [GITIGNORED]
    - add cloud shadows on terrain [GITIGNORED]
    - change: landscape material with auto generated (custom modified with road + grid)
    - attempt to fix circular dependencies (WIP)

---
+++ Source/MasterArbeit/GMInGame.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CharacterTurnStart.h"
+#include "GameFramework/GameModeBase.h"
+#include "GMInGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MASTERARBEIT_API AGMInGame : public AGameModeBase
+{
+	GENERATED_BODY()
+
+	AGMInGame(const FObjectInitializer& ObjectInitializer);
+
+	virtual void BeginPlay() override;
+
+private:
+	void SpawnCharacters();
+	APawn* SpawnCharacterPawnAtCharacterStart(const ConstructorHelpers::FClassFinder<APawn>& ClassFinder, const FTransform& SpawnTransform);
+};
