7bf33759291bb3cdb03d8477b47fff7aedba54b2
Jofresh77 <56953251+Jofresh77@users.noreply.github.com>
2025-08-04 16:47:48 +0200

    first interactive dialog model to game pipeline
    - note: very slow execution
       1. small model?
       2. unoptimized model?
       3. max response length to high overall? Model should definitely output less
    - make a developerSetting class and transfer all configuration and setting parameters for LlamaRunner
       -> no more blueprint class and c++ remove abstract and query project settings to configure?
    - resolve the ever calling state task for character prompt reasoning
    - transfer from BP to C++ for all tasks

---
+++ Source/MasterArbeit/AI/STTasks/STTPromptCharacterTurnReasoning.cpp
@@ -0,0 +1,93 @@
+﻿#include "STTPromptCharacterTurnReasoning.h"
+
+#include "LlamaCppSubsystem.h"
+#include "StateTreeExecutionContext.h"
+#include "Kismet/GameplayStatics.h"
+
+#define LOCTEXT_NAMESPACE "StateTree"
+
+//TODO FOR SOME REASON IT'S CALLED EVERY TICK ? -> FIX
+EStateTreeRunStatus FStateTreePromptCharacterTurnReasoningTask::EnterState(
+	FStateTreeExecutionContext& Context,
+	const FStateTreeTransitionResult& Transition) const
+{
+	FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+
+	if (!InstanceData.ChatHistoryDataTable.IsValid()
+		|| InstanceData.ChatHistoryDataTable->GetRowNames().Num() <= 0)
+	{
+		return EStateTreeRunStatus::Failed;
+	}
+
+	TArray<FChatMessage> ChatHistory;
+
+	TArray<FChatMessage*> ChatHistoryDataTableRows;
+	InstanceData.ChatHistoryDataTable->GetAllRows<FChatMessage>("", ChatHistoryDataTableRows);
+	for (FChatMessage* Row : ChatHistoryDataTableRows)
+	{
+		if (Row)
+		{
+			ChatHistory.Add(*Row);
+		}
+	}
+
+	FChatMessage LastUserPrompt = ChatHistory.Pop();
+	if (LastUserPrompt.ChatRole != User)
+	{
+		return EStateTreeRunStatus::Failed;
+	}
+
+	bool bFoundAndExecuted = false;
+
+	// can't use GameInstance->GetSubsystem<...> because it returns the abstract parent class and not the bp subclass
+	UGameplayStatics::GetGameInstance(Context.GetOwner())->ForEachSubsystem<UGameInstanceSubsystem>(
+		[&ChatHistory, &InstanceData, &LastUserPrompt, &bFoundAndExecuted](UGameInstanceSubsystem* Subsystem)
+		{
+			if (ULlamaCppSubsystem* LlamaRunnerSubsystem = Cast<ULlamaCppSubsystem>(Subsystem))
+			{
+				LlamaRunnerSubsystem->AddChatHistory(
+					ChatHistory,
+					InstanceData.SystemPromptDataAsset->SystemPrompt.ToString(),
+					true);
+
+				LlamaRunnerSubsystem->AsyncProcessUserPrompt(
+					LastUserPrompt.Content,
+					"");
+
+				bFoundAndExecuted = true;
+
+				// stop iterating once found
+				return false;
+			}
+
+			// continue iterating
+			return true;
+		});
+
+	if (!bFoundAndExecuted)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to find LlamaCppSubsystem"));
+		return EStateTreeRunStatus::Failed;
+	}
+
+	return EStateTreeRunStatus::Running;
+}
+
+#if WITH_EDITOR
+FText FStateTreePromptCharacterTurnReasoningTask::GetDescription(const FGuid& ID, FStateTreeDataView InstanceDataView,
+                                                                 const IStateTreeBindingLookup& BindingLookup,
+                                                                 EStateTreeNodeFormatting Formatting) const
+{
+	const FInstanceDataType* InstanceData = InstanceDataView.GetPtr<FInstanceDataType>();
+	check(InstanceData);
+
+	const FText Format = (Formatting == EStateTreeNodeFormatting::RichText)
+		                     ? LOCTEXT("PromptCharacterTurnReasoningRich", "<b>Prompt Model</> \"{Text}\"")
+		                     : LOCTEXT("PromptCharacterTurnReasoning", "Prompt Model \"{Text}\"");
+
+	return FText::FormatNamed(Format,
+	                          TEXT("Text"), LOCTEXT("with Character narrative", "with Character narrative"));
+}
+#endif
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/MasterArbeit/TextCommunication/ModelResponseParser.cpp
@@ -0,0 +1,112 @@
+﻿#include "ModelResponseParser.h"
+#include "Dom/JsonObject.h"
+#include "Serialization/JsonReader.h"
+#include "JsonObjectWrapper.h"
+#include "Serialization/JsonSerializer.h"
+
+bool UModelResponseParser::ParseJsonFromString(const FString& JsonString,
+									FJsonObjectWrapper& OutJsonObject,
+									FString& OutErrorMessage)
+{
+	TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(JsonString);
+
+	if (!FJsonSerializer::Deserialize(Reader, OutJsonObject.JsonObject) || !OutJsonObject.JsonObject.IsValid())
+	{
+		OutErrorMessage = "Failed to parse JSON string";
+		return false;
+	}
+
+	return true;
+}
+
+bool UModelResponseParser::ParseVerbalInteractionFromJson(const FJsonObjectWrapper& JsonObjectWrapper,
+                                                          FVerbalInteraction& OutVerbalInteraction,
+                                                          FString& OutErrorMessage)
+{
+	const TSharedPtr<FJsonObject>* VerbalInteractionObject;
+	if (!JsonObjectWrapper.JsonObject->TryGetObjectField(FString("verbal-interaction"), VerbalInteractionObject))
+	{
+		OutErrorMessage = "verbal-interaction field not found in JSON";
+		return false;
+	}
+
+	FString ResponseTypeString;
+	if ((*VerbalInteractionObject)->TryGetStringField(FString("response-type"), ResponseTypeString))
+	{
+		OutVerbalInteraction.ResponseType = StringToResponseType(ResponseTypeString);
+	}
+	else
+	{
+		OutErrorMessage = "response-type field not found or invalid";
+		return false;
+	}
+
+	if (!(*VerbalInteractionObject)->TryGetStringField(FString("content"), OutVerbalInteraction.Content))
+	{
+		OutErrorMessage = "content field not found or invalid";
+		return false;
+	}
+
+	if (OutVerbalInteraction.Content.Len() < 1000)
+	{
+		OutErrorMessage = FString::Printf(TEXT("content length is %d, but minimum required is 1000"),
+		                                  OutVerbalInteraction.Content.Len());
+	}
+
+	FString EmotionalToneString;
+	if ((*VerbalInteractionObject)->TryGetStringField(FString("emotional_tone"), EmotionalToneString))
+	{
+		OutVerbalInteraction.EmotionalTone = StringToEmotionalTone(EmotionalToneString);
+	}
+	else
+	{
+		OutErrorMessage = "emotional_tone field not found or invalid";
+		return false;
+	}
+
+	if (OutVerbalInteraction.ResponseType == EVerbalResponseType::Invalid)
+	{
+		OutErrorMessage = "Invalid response-type value: " + ResponseTypeString;
+		return false;
+	}
+
+	if (OutVerbalInteraction.EmotionalTone == EEmotionalTone::Invalid)
+	{
+		OutErrorMessage = "Invalid emotional_tone value: " + EmotionalToneString;
+		return false;
+	}
+
+	OutErrorMessage = "Success";
+	return true;
+}
+
+EVerbalResponseType UModelResponseParser::StringToResponseType(const FString& TypeString)
+{
+	static const TMap<FString, EVerbalResponseType> ResponseTypeMap = {
+		{TEXT("speech"), EVerbalResponseType::Speech},
+		{TEXT("silence"), EVerbalResponseType::Silence}
+	};
+
+	const EVerbalResponseType* Found = ResponseTypeMap.Find(TypeString.ToLower());
+	return Found ? *Found : EVerbalResponseType::Invalid;
+}
+
+EEmotionalTone UModelResponseParser::StringToEmotionalTone(const FString& ToneString)
+{
+	static const TMap<FString, EEmotionalTone> EmotionalToneMap = {
+		{TEXT("enthusiasm"), EEmotionalTone::Enthusiasm},
+		{TEXT("conservatism"), EEmotionalTone::Conservatism},
+		{TEXT("boredom"), EEmotionalTone::Boredom},
+		{TEXT("antagonism"), EEmotionalTone::Antagonism},
+		{TEXT("pain"), EEmotionalTone::Pain},
+		{TEXT("anger"), EEmotionalTone::Anger},
+		{TEXT("no-sympathy"), EEmotionalTone::NoSympathy},
+		{TEXT("fear"), EEmotionalTone::Fear},
+		{TEXT("sympathy"), EEmotionalTone::Sympathy},
+		{TEXT("grief"), EEmotionalTone::Grief},
+		{TEXT("apathy"), EEmotionalTone::Apathy}
+	};
+
+	const EEmotionalTone* Found = EmotionalToneMap.Find(ToneString.ToLower());
+	return Found ? *Found : EEmotionalTone::Invalid;
+}
