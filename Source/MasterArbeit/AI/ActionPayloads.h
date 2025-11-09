2c01eb78bad772c8b168cd5bce4b6309657723fb
Jofresh77 <56953251+Jofresh77@users.noreply.github.com>
2025-11-09 02:15:05 +0100

    Agents can seek player
    - restructure project files (gameplay abilities and effects)
    - add: enums, wrappers and abilities for the move actions (state tree events, GAS events)
    - implement: seek movement action -> use AIC move to location for now
    - very simplified version, establish how the system will work

---
+++ Source/MasterArbeit/AI/ActionPayloads.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "ActionPayloads.generated.h"
+
+USTRUCT(BlueprintType, Category="LlamaMovement | ActionPayload")
+struct FMovementAction
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Distance = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	AActor* Target = nullptr;
+};
+
+UCLASS(Blueprintable, Category="LlamaMovement | AbilityPayload")
+class UMovementActionWrapper : public UObject
+{
+	GENERATED_BODY()
+
+	UMovementActionWrapper()
+	{
+	}
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(ExposeOnSpawn=true),
+		Category="LlamaMovement | AbilityPayload")
+	FMovementAction MovementAction;
+
+	UFUNCTION(BlueprintCallable, Category="LlamaMovement | AbilityPayload")
+	static UMovementActionWrapper* CreateWrapper(UObject* Outer, const FMovementAction& Action);
+};
