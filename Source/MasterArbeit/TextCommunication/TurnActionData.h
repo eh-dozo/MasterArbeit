524645f33862fe83c64c41872a75acc14363ee83
Jofresh77 <56953251+Jofresh77@users.noreply.github.com>
2025-11-10 01:01:56 +0100

    Track turn data
    - add struct for tracking
    - add: BP array of that struct to allow BP Map variable <Base_Character, Array<that struct>>
    - create vars in GM that will be used for the tracking

---
+++ Source/MasterArbeit/TextCommunication/TurnActionData.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "MasterArbeit/MasterArbeitCharacter.h"
+#include "TurnActionData.generated.h"
+
+USTRUCT(BlueprintType, Category="TurnActionData")
+struct FTurnActionData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 TurnNumber;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString MovementDescription;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString VerbalInteraction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FString> AbilitiesUsed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Location;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FString> NPCsInRange;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Terrain;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString TimeOfDay;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Weather;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TEnumAsByte<ECharacterGroupName> ActingCharacter;
+};
