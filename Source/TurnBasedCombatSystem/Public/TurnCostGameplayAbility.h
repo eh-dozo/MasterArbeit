bc1914ab84a7d30fc961f939540319f646bb01fb
Jofresh77 <56953251+Jofresh77@users.noreply.github.com>
2025-11-16 02:34:55 +0100

    Display PA PM + Player Movement as actions

--- Source/TurnBasedCombatSystem/Public/TurnCostGameplayAbility.h
+++ Source/TurnBasedCombatSystem/Public/TurnCostGameplayAbility.h
@@ -16,7 +16,25 @@ class TURNBASEDCOMBATSYSTEM_API UTurnCostGameplayAbility : public UGameplayAbili
 
 	UTurnCostGameplayAbility(const FObjectInitializer& ObjectInitializer);
 
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
 public:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	float Cost;
+
+protected:
+	float EffectiveCost;
+
+	virtual bool CheckCost(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, OUT FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
+	virtual void ApplyCost(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const override;
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Turn Cost Gameplay Ability")
+	float GetEffectiveCost() const { return EffectiveCost; }
+
+	UFUNCTION(BlueprintCallable, Category = "Turn Cost Gameplay Ability")
+	void SetEffectiveCost(const float NewValue) { EffectiveCost = NewValue; }
+
+private:
+	mutable FGameplayEffectSpecHandle CachedCostEffectSpec;
 };
